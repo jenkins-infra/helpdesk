68eb919711cae648099afaf0c3a48730154a6b8a
Hervé Le Meur <hlemeur@cloudbees.com>
2023-02-24 16:46:14 +0100

    better comment

--- scripts/helpdesk3402-pin-gha-with-sha1/pin-gha-with-sha1.sh
+++ scripts/helpdesk3402-pin-gha-with-sha1/pin-gha-with-sha1.sh
@@ -6,11 +6,11 @@ find .github/workflows -maxdepth 1 -type f -name "*.y*" -exec /tmp/helpdesk3402/
 # remove 'ratchet:org/repo@' from the comment to keep only the version
 find .github/workflows -maxdepth 1 -type f -name "*.y*" -exec sed -i '' -r "s/# ratchet:[a-z0-9\/-]+@/# /g" {} \;
 
-# don't pin official GitHub actions (in 'action' org)
+# don't use SHA-1 for official GitHub actions (in 'action' org)
 find .github/workflows -maxdepth 1 -type f -name "*.y*" -exec sed -i '' -r "s/uses: \"?(actions\/[a-z0-9-]+@)[a-z0-9]+\"? # /uses: \1/g" {} \;
 
-# don't pin official GitHub actions (in 'github' org)
+# don't use SHA-1 for official GitHub actions (in 'github' org)
 find .github/workflows -maxdepth 1 -type f -name "*.y*" -exec sed -i '' -r "s/uses: \"?(github\/[a-z0-9-]+@)[a-z0-9]+\"? # /uses: \1/g" {} \;
 
-# don't pin official Jenkins actions (in 'jenkins-infra' org)
+# don't use SHA-1 for official Jenkins actions (in 'jenkins-infra' org)
 find .github/workflows -maxdepth 1 -type f -name "*.y*" -exec sed -i '' -r "s/uses: \"?(jenkins-infra\/[a-z0-9-]+@)[a-z0-9]+\"? # /uses: \1/g" {} \;
